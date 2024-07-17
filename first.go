9d57ded8925c87a7c822ed9c15cb5f8c8f9d21d6
Samuel Sobreira <samuel.sobreira@camara.leg.br>
2024-07-17 16:52:20 -0300

    Primeiro estudo adicionado

---
+++ first.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+
+func main() {
+  nome := "João"
+  num := 8
+  
+  
+  if (num < 7){
+      fmt.Println("Nome :", nome,"Idade :", num)
+  }
+  
+  for i:=80;i>0;i--{
+      fmt.Println(i)
+  }
+}
+
